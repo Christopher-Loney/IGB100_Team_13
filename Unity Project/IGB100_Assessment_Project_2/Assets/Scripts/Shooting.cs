00120988d2765d75f95688a0b5016e712b6086aa
Christopher-Loney <85321705+Christopher-Loney@users.noreply.github.com>
2022-05-03 16:56:40 +1000

    fixes to the shooting script

--- Unity Project/IGB100_Assessment_Project_2/Assets/Scripts/Shooting.cs
+++ Unity Project/IGB100_Assessment_Project_2/Assets/Scripts/Shooting.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public string noiseMakerTag;
-    public float shootingRange;
-    private Camera cam;
+    public string noiseMakerTag; // tag used on the noisemakers
+    public float shootingRange; // how far the player can shoot
+    private Camera cam; // the camera on the player
     // Start is called before the first frame update
     void Start()
     {
+        //get the only camera in the scene
         cam = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //when the player releses the fire1 button
         if(Input.GetButtonUp("Fire1"))
         {
             Debug.Log("shoot");
             RaycastHit hit;
-
-            if(Physics.Raycast(cam.transform.position, Vector3.forward, out hit, shootingRange))
+            Vector3 fwd = cam.transform.forward;
+            if(Physics.Raycast(cam.transform.position, fwd, out hit, shootingRange))
             {
-                Debug.Log(hit.collider.gameObject.name);
+                Debug.Log(hit.collider.name);
                 if (hit.collider.gameObject.tag == noiseMakerTag)
                 {
                     Destroy(hit.collider.gameObject);
