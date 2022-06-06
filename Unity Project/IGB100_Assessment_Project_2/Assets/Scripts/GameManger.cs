8e3eabbfbcb422013f2f8d7fe4748ab46296f935
Christopher-Loney <85321705+Christopher-Loney@users.noreply.github.com>
2022-06-06 22:42:10 +1000

    Timer fix/ ammo counter/ UI
    - fixed the timer
    - added the ammo counter and reload
    - added the UI for the ammo counter
    - added the UI into the ranch scene

--- Unity Project/IGB100_Assessment_Project_2/Assets/Scripts/GameManger.cs
+++ Unity Project/IGB100_Assessment_Project_2/Assets/Scripts/GameManger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManger : MonoBehaviour
@@ -8,6 +9,9 @@ public class GameManger : MonoBehaviour
     public List<GameObject> noiseMakers;
     public GameObject player;
     public string nextScene;
+    [Tooltip("time to complete the level in seconds")]
+    public float timer;
+    public Text timerText;
     private CursorSettings CursorSettings;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,14 @@ public class GameManger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            Debug.Log("GameOver");
+        }
+        int minutes = (int)timer / 60;
+        int seconds = (int)timer % 60;
+        timerText.text = minutes + ":" + seconds;
         if(noiseMakers.Count < 1)
         {
             SceneManager.LoadScene(nextScene);
--- Unity Project/IGB100_Assessment_Project_2/Assets/Scripts/Shooting.cs
+++ Unity Project/IGB100_Assessment_Project_2/Assets/Scripts/Shooting.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Shooting : MonoBehaviour
 {
     public string noiseMakerTag; // tag used on the noisemakers
     public float shootingRange; // how far the player can shoot
+    public int ammo = 6; // the players ammo count
+    public List<Image> casings;
+    public float reloadTime;
+    public ParticleSystem bubbles;
+    public AudioSource gunShot;
     private Camera cam; // the camera on the player
     private GameManger gameManger;// the game manager
     // Start is called before the first frame update
@@ -20,10 +25,31 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //when the player releses the fire1 button
-        if(Input.GetButtonUp("Fire1"))
+        if(Input.GetButtonUp("Fire1") && ammo > 0)
         {
             Debug.Log("shoot");
+            ammo--;
+            if (bubbles.isPlaying)
+            {
+                bubbles.Stop();
+                bubbles.Play();
+            }
+            else
+            {
+                bubbles.Play();
+            }
+            if(gunShot.isPlaying)
+            {
+                gunShot.Stop();
+                gunShot.Play();
+            }
+            else
+            {
+                gunShot.Play();
+            }
+            casings[ammo].gameObject.SetActive(false);
             RaycastHit hit;
             Vector3 fwd = cam.transform.forward;
             if(Physics.Raycast(cam.transform.position, fwd, out hit, shootingRange))
@@ -37,6 +63,20 @@ public class Shooting : MonoBehaviour
                 }
                 
             }
+            if(ammo <= 0)
+            {
+                StartCoroutine(Reload());
+            }
         }
+    }
+    public IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        ammo = 6;
+        for (int i = 0; i < casings.Count; i++)
+        {
+            casings[i].gameObject.SetActive(true);
+        }
+        StopCoroutine(Reload());
     }
 }
